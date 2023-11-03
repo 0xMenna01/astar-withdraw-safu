63036ece1a89b974a308a8bba353015ab83a8a08
Emanuele Valzano <menna0x@macbook-pro.station>
2023-11-03 18:59:28 +0100

    Version 2

--- config.ts
+++ config.ts
@@ -1,4 +1,5 @@
 export const config = {
-    seedPhrase: '',
-    recievingAddress: '',
+    seedPhrase:
+        'manage civil delay crush return list dice salon audit cause soul cherry',
+    recievingAddress: 'ZNooREZRsM8tbhghsy5vzgnP4uyABHKp8K8k52b97KJWjCd',
 }
