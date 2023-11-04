742d296be367223d9e40bc155a65201b47528325
0xMenna01 <menna0x@macbook-pro.homenet.telecomitalia.it>
2023-11-04 14:52:06 +0100

    Final implementation - removing config data

--- config.ts
+++ config.ts
@@ -1,5 +1,4 @@
 export const config = {
-    seedPhrase:
-        'toe spell acid leg scatter danger expire require decrease iron spare margin',
-    recievingAddress: '5GHLbwns8Y7svUmx1qTa5RjGHNqit1SAmxpfMJM2cRbm98ud',
+    seedPhrase: '',
+    recievingAddress: '',
 }
